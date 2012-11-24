101b9b80e148d5e5c68d5833b464bf510af4c142
Manoj Kumar <manojgumber@gmail.com>
2012-11-24 18:39:16 +0530

    fixed a flaw in logic for distinction b/w prefix words that are present/non present

--- ds/trie.py
+++ ds/trie.py
@@ -1,24 +1,25 @@
 
 class Tries:
+	leaf_char='^'
+
 	"""Data Structure Tries"""
 	def __init__(self):
 		self.root = {}
 
 	def add_word(self,word):
+		word += Tries.leaf_char
 		next_level = self.root
- 		for letter in w:
+ 		for letter in word:
 			next_level = next_level.setdefault(letter,{})
 
 	def find_word(self,word):
+		word += Tries.leaf_char
 		next_level = self.root
 		for letter in word:
 			next_level = next_level.get(letter,None)
 			if next_level is None:
 				return False
-		if next_level:
-			return False
-		else:
-			return True
+		return True
 
 	def __str__(self):
 		import pprint
@@ -27,7 +28,7 @@ class Tries:
 
 
 tries = Tries() 
-words = [ "microsoft", "microscope", "amazon","america","amex","minatory","miracle"]
+words = [ "microsoft", "microscope", "amazon","america","amex","minatory","miracle","micro"]
 
 for w in words:
 	tries.add_word(w)
@@ -37,4 +38,6 @@ assert tries.find_word("sexy") is False
 assert tries.find_word("microscope") is True
 assert tries.find_word("mina") is False
 assert tries.find_word("miracle") is True
+assert tries.find_word("micro") is True
+
 
